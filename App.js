f8f51a0e9cf31741f0b33c6df57f3090464835f6
Young-BLUE <youngblue.coding@gmail.com>
2023-01-03 20:49:35 +0900

    애니메이션을 직접 구현하면 이렇게 단순한 움직임에 더러운 코드가 생긴다

--- App.js
+++ App.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
 export default function App() {
-  return null;
+  const [y, setY] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+  const [way, setWay] = useState(false);
+  const moveUp = () => {
+    if (!way) {
+      setIntervalId(setInterval(() => setY((prev) => prev + 5), 10));
+    } else {
+      setIntervalId(setInterval(() => setY((prev) => prev - 5), 10));
+    }
+  };
+  useEffect(() => {
+    if (!way && y === 200) {
+      clearInterval(intervalId);
+      setWay(!way);
+    } else if (way && y === 0) {
+      clearInterval(intervalId);
+      setWay(!way);
+    }
+  }, [y, intervalId]);
+  return (
+    <Container>
+      <Box
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: y }],
+        }}
+      />
+    </Container>
+  );
 }
