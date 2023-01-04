0a719e2d7bff7707984f3c489ab5eea9c4031aee
Young-BLUE <youngblue.coding@gmail.com>
2023-01-04 22:23:20 +0900

    react-native의 Animated 사용해서 애니메이션 효과 적용하기 3 useRef를 이용하여 컴포넌트의 리렌더링에서 벗어나 y 값을 이용한다

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Animated, TouchableOpacity } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, Easing, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -26,17 +26,18 @@ const AnimatedBox1 = styled(Animated.createAnimatedComponent(TouchableOpacity))`
 const AnimatedBox2 = Animated.createAnimatedComponent(Box);
 
 export default function App() {
+  const [up, setUp] = useState(false);
+  const toggleUp = () => setUp((prev) => !prev);
+  const Y = useRef(new Animated.Value(0)).current; // 4. useRef 를 통해 state 가 변경될 때 re-rendering 을 방지하고 값을 그대로 이용. (component의 lifeTime 동안 Object가 지속되도록 해준다)
   // 1. animation 만들고 싶은 value 는 state로 만들지 않는다.
-  const Y = new Animated.Value(0);
   // 2. value를 직접 set 하지 않는다.
   // 3. Animatable Components 에만 animation을 줄 수 있다.
   // 3-1. 그러나 필요하다면 createAnimatedComponent 메소드를 통해 만들 수 있다.
   const moveUp = () => {
-    Animated.spring(Y, {
-      toValue: 200,
-      bounciness: 15,
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
       useNativeDriver: true, // bridge 사용 없이 native 로 애니메이션 정보를 보내게 된다 (Android, iOS)
-    }).start(); // React Component 에서 실행되지 않기 때문에 re-rendering 이 일어나지 않는다. 밖에서 console 찍어보면 Y 값은 그대로. addEventListener 하면 값 보임
+    }).start(toggleUp); // React Component 에서 실행되지 않기 때문에 re-rendering 이 일어나지 않는다. 밖에서 console 찍어보면 Y 값은 그대로. addEventListener 하면 값 보임
   };
 
   return (
