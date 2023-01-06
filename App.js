976e337bc5a0363ca8695fb8b8114fc855e57946
Young-BLUE <youngblue.coding@gmail.com>
2023-01-06 22:35:27 +0900

    react-native의 Animated 사용해서 애니메이션 효과 적용하기 6 화면의 구석 4군데를 돌아다니는 무한루프 애니메이션

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, Easing, Pressable, TouchableOpacity } from "react-native";
+import {
+  Animated,
+  Dimensions,
+  Easing,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -25,26 +31,53 @@ const AnimatedBox1 = styled(Animated.createAnimatedComponent(TouchableOpacity))`
 // 방법 2. 생성된 styled 객체를 Animated화 (TouchableOpacity를 Animated, 지금은 View로 바꾼상태)
 const AnimatedBox2 = Animated.createAnimatedComponent(Box);
 
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+
 export default function App() {
-  const [up, setUp] = useState(false);
-  const toggleUp = () => setUp((prev) => !prev);
-  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current; // 4. useRef 를 통해 state 가 변경될 때 re-rendering 을 방지하고 값을 그대로 이용. (component의 lifeTime 동안 Object가 지속되도록 해준다)
+  const POSITION = useRef(
+    new Animated.ValueXY({
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    })
+  ).current; // 4. useRef 를 통해 state 가 변경될 때 re-rendering 을 방지하고 값을 그대로 이용. (component의 lifeTime 동안 Object가 지속되도록 해준다)
   // 1. animation 만들고 싶은 value 는 state로 만들지 않는다.
   // 2. value를 직접 set 하지 않는다.
   // 3. Animatable Components 에만 animation을 줄 수 있다.
   // 3-1. 그러나 필요하다면 createAnimatedComponent 메소드를 통해 만들 수 있다.
-  const moveUp = () => {
-    Animated.timing(POSITION, {
-      toValue: up ? 300 : -300,
-      useNativeDriver: false, // bridge 사용 없이 native 로 애니메이션 정보를 보내게 된다 (Android, iOS)
-      duration: 1000,
-    }).start(toggleUp); // React Component 에서 실행되지 않기 때문에 re-rendering 이 일어나지 않는다. 밖에서 console 찍어보면 POSITION 값은 그대로. addEventListener 하면 값 보임
-  };
-  const rotation = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["-360deg", "360deg"],
+  const topLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
   });
-
+  const bottomLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const bottomRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const topRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+  const moveUp = () => {
+    // React Component 에서 실행되지 않기 때문에 re-rendering 이 일어나지 않는다. 밖에서 console 찍어보면 POSITION 값은 그대로. addEventListener 하면 값 보임
+    Animated.loop(
+      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
+    ).start();
+  };
   const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
@@ -59,7 +92,6 @@ export default function App() {
     outputRange: ["rgb(255, 99, 71)", "rgb(71, 155, 255)"],
   });
 
-  POSITION.addListener(() => console.log(POSITION.getTranslateTransform()));
   return (
     <Container>
       <Pressable onPress={moveUp}>
@@ -67,10 +99,7 @@ export default function App() {
           style={{
             borderRadius,
             backgroundColor: bgColor,
-            transform: [
-              { rotateY: rotation },
-              ...POSITION.getTranslateTransform(),
-            ],
+            transform: [...POSITION.getTranslateTransform()],
           }}
         />
       </Pressable>
