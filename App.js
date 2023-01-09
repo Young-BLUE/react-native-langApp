a09c1ae12f020aebf981e91f979e175bf06476f4
Young-BLUE <youngblue.coding@gmail.com>
2023-01-09 21:11:06 +0900

    react-native의 Animated 사용해서 애니메이션 효과 적용하기 9 position x에 따른 rotation 적용과 일정 x 넘어가면 화면 밖으로 날리는 효과 추가

--- App.js
+++ App.js
@@ -21,41 +21,61 @@ const Card = styled(Animated.createAnimatedComponent(View))`
 `;
 
 export default function App() {
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderGrant: () => onPressIn(),
-      onPanResponderMove: (evt, { dx }) => {
-        position.setValue(dx);
-      },
-      onPanResponderRelease: () => {
-        Animated.parallel([
-          onPressOut,
-          Animated.spring(position, {
-            toValue: 0,
-            useNativeDriver: true,
-          }),
-        ]).start();
-      },
-    })
-  ).current;
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
-  const onPressIn = () =>
-    Animated.spring(scale, {
-      toValue: 0.95,
-      useNativeDriver: true,
-    }).start();
+  const rotation = position.interpolate({
+    inputRange: [-280, 280], // 한계치를 넘어서도 작동하는 문제가 있음
+    outputRange: ["-10deg", "10deg"],
+    extrapolate: "clamp", // 한계치를 넘었을 때 처리 명세 하는 부분 (extend, clamp...)
+  });
   const onPressOut = Animated.spring(scale, {
     toValue: 1,
     useNativeDriver: true,
   });
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.95,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (evt, { dx }) => {
+        console.log(dx);
+        position.setValue(dx);
+      },
+      onPanResponderGrant: () => onPressIn.start(),
+      onPanResponderRelease: (evt, { dx }) => {
+        if (dx < -300) {
+          Animated.spring(position, {
+            toValue: -500,
+            useNativeDriver: true,
+          }).start();
+        } else if (dx > 300) {
+          Animated.spring(position, {
+            toValue: 500,
+            useNativeDriver: true,
+          }).start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
+      },
+    })
+  ).current;
+
   return (
     <Container>
       <Card
         {...panResponder.panHandlers}
         style={{
-          transform: [{ scale }, { translateX: position }],
+          transform: [
+            { scale },
+            { translateX: position },
+            { rotateZ: rotation },
+          ],
         }}
       >
         <Ionicons name={"pizza"} color={"#192a56"} size={98} />
