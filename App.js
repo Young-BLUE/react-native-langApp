283df4d0a4b0c51dd6f7a942f12bab981c3e8d51
Young-BLUE <youngblue.coding@gmail.com>
2023-01-12 18:58:54 +0900

    Card Project 6 애니메이션으로 카드 움직이는 언어배우기 앱 기초

--- App.js
+++ App.js
@@ -4,134 +4,110 @@ import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import icons from "./icons";
 
+const BLACK_COLOR = "#1e272e";
+const GREY = "#485460";
+const GREEN = "#2ecc71";
+const RED = "#e74c3c";
+
+const Wrapper = styled.View`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
+`;
 const Container = styled.View`
+  flex: 1;
+  margin-top: 20px;
+`;
+const Edge = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #00a8ff;
+  //background-color: ${RED};
 `;
-
-const CardContainer = styled.View`
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 150px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${GREY};
+  border-radius: 50px;
+`;
+const Word = styled.Text`
+  font-size: 38px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+const Center = styled.View`
   flex: 3;
   justify-content: center;
   align-items: center;
 `;
 
-const Card = styled(Animated.createAnimatedComponent(View))`
+const IconCard = styled(Animated.createAnimatedComponent(View))`
   background-color: white;
-  width: 300px;
-  height: 300px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 12px;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
-  position: absolute;
-`;
-
-const Button = styled.TouchableOpacity`
-  margin: 0px 10px;
-`;
-
-const ButtonContainer = styled.View`
-  flex-direction: row;
-  flex: 1;
+  padding: 10px 20px;
+  border-radius: 10px;
 `;
 
 export default function App() {
+  // Values
   const scale = useRef(new Animated.Value(1)).current;
-  const position = useRef(new Animated.Value(0)).current;
-  const rotation = position.interpolate({
-    inputRange: [-280, 280], // 한계치를 넘어서도 작동하는 문제가 있음
-    outputRange: ["-10deg", "10deg"],
-    extrapolate: "clamp", // 한계치를 넘었을 때 처리 명세 하는 부분 (extend, clamp...)
-  });
-  const secondCardScale = position.interpolate({
-    inputRange: [-300, 0, 300],
-    outputRange: [1, 0.7, 1],
-    extrapolate: "clamp",
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  // Animations
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
   });
   const onPressOut = Animated.spring(scale, {
     toValue: 1,
     useNativeDriver: true,
   });
-  const onPressIn = Animated.spring(scale, {
-    toValue: 0.95,
-    useNativeDriver: true,
-  });
-  const goCenter = Animated.spring(position, {
+  const goHome = Animated.spring(position, {
     toValue: 0,
+    tension: 10,
     useNativeDriver: true,
   });
-  const goLeft = Animated.spring(position, {
-    toValue: -500,
-    tension: 5, // 넘어가는 속도 조절
-    useNativeDriver: true,
-    restSpeedThreshold: 100,
-    restDisplacementThreshold: 100,
-  });
-  const goRight = Animated.spring(position, {
-    toValue: 500,
-    tension: 5,
-    useNativeDriver: true,
-  });
+  // PanResponder
   const panResponder = useRef(
     PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (evt, { dx }) => {
-        position.setValue(dx);
+      onStartShouldSetPanResponder: () => true, // 손가락 움직임 감지
+      onPanResponderMove: (evt, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
       },
-      onPanResponderGrant: () => onPressIn.start(),
-      onPanResponderRelease: (evt, { dx }) => {
-        if (dx < -250) {
-          goLeft.start(onDismiss);
-        } else if (dx > 250) {
-          goRight.start(onDismiss);
-        } else {
-          Animated.parallel([onPressOut, goCenter]).start();
-        }
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderRelease: () => {
+        Animated.parallel([onPressOut, goHome]).start();
       },
     })
   ).current;
 
-  const [index, setIndex] = useState(0);
-  const onDismiss = () => {
-    scale.setValue(1);
-    position.setValue(0);
-    setIndex((prev) => prev + 1);
-  };
+  // State
 
   return (
-    <Container>
-      <CardContainer>
-        <Card
-          style={{
-            transform: [{ scale: secondCardScale }],
-          }}
-        >
-          <Ionicons name={icons[index + 1]} color={"#192a56"} size={98} />
-        </Card>
-        <Card
-          {...panResponder.panHandlers}
-          style={{
-            transform: [
-              { scale },
-              { translateX: position },
-              { rotateZ: rotation },
-            ],
-          }}
-        >
-          <Ionicons name={icons[index]} color={"#192a56"} size={98} />
-        </Card>
-      </CardContainer>
-
-      <ButtonContainer>
-        <Button onPress={() => goLeft.start(onDismiss)}>
-          <Ionicons name={"close-circle"} color={"white"} size={58} />
-        </Button>
-        <Button onPress={() => goRight.start(onDismiss)}>
-          <Ionicons name={"checkmark-circle"} color={"white"} size={58} />
-        </Button>
-      </ButtonContainer>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Edge>
+          <WordContainer>
+            <Word color={GREEN}>내 안다</Word>
+          </WordContainer>
+        </Edge>
+        <Center>
+          <IconCard
+            {...panResponder.panHandlers}
+            style={{
+              transform: [...position.getTranslateTransform(), { scale }],
+            }}
+          >
+            <Ionicons name={"beer"} color={GREY} size={80} />
+          </IconCard>
+        </Center>
+        <Edge>
+          <WordContainer>
+            <Word color={RED}>내 모른다</Word>
+          </WordContainer>
+        </Edge>
+      </Container>
+    </Wrapper>
   );
 }
---
+++ App3.js
@@ -0,0 +1,111 @@
+import React, { useRef, useState } from "react";
+import { Animated, PanResponder, View } from "react-native";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #00a8ff;
+`;
+
+export default function App3() {
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-280, 280], // 한계치를 넘어서도 작동하는 문제가 있음
+    outputRange: ["-10deg", "10deg"],
+    extrapolate: "clamp", // 한계치를 넘었을 때 처리 명세 하는 부분 (extend, clamp...)
+  });
+  const secondCardScale = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.7, 1],
+    extrapolate: "clamp",
+  });
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.95,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  const goLeft = Animated.spring(position, {
+    toValue: -500,
+    tension: 5, // 넘어가는 속도 조절
+    useNativeDriver: true,
+    restSpeedThreshold: 100,
+    restDisplacementThreshold: 100,
+  });
+  const goRight = Animated.spring(position, {
+    toValue: 500,
+    tension: 5,
+    useNativeDriver: true,
+  });
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (evt, { dx }) => {
+        position.setValue(dx);
+      },
+      onPanResponderGrant: () => onPressIn.start(),
+      onPanResponderRelease: (evt, { dx }) => {
+        if (dx < -250) {
+          goLeft.start(onDismiss);
+        } else if (dx > 250) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
+      },
+    })
+  ).current;
+
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    scale.setValue(1);
+    position.setValue(0);
+    setIndex((prev) => prev + 1);
+  };
+
+  return (
+    <Container>
+      <CardContainer>
+        <Card
+          style={{
+            transform: [{ scale: secondCardScale }],
+          }}
+        >
+          <Ionicons name={icons[index + 1]} color={"#192a56"} size={98} />
+        </Card>
+        <Card
+          {...panResponder.panHandlers}
+          style={{
+            transform: [
+              { scale },
+              { translateX: position },
+              { rotateZ: rotation },
+            ],
+          }}
+        >
+          <Ionicons name={icons[index]} color={"#192a56"} size={98} />
+        </Card>
+      </CardContainer>
+
+      <ButtonContainer>
+        <Button onPress={() => goLeft.start(onDismiss)}>
+          <Ionicons name={"close-circle"} color={"white"} size={58} />
+        </Button>
+        <Button onPress={() => goRight.start(onDismiss)}>
+          <Ionicons name={"checkmark-circle"} color={"white"} size={58} />
+        </Button>
+      </ButtonContainer>
+    </Container>
+  );
+}
